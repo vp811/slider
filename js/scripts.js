d632ff2a65361f5eaa1255bb4cf5e5fce1f28d47
Efren Vasquez <e.vasquez@ufl.edu>
2021-11-30 17:04:46 -0600

    live class changes

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,9 @@ $(document).ready(function(){
         infinite: true,
         speed: 300,
         slidesToShow: 1,
-        adaptiveHeight: true
+        adaptiveHeight: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
     });
   });
           
